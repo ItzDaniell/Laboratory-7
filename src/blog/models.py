1871d7138b04e0c307f955854f4bf3696b0846ec
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-04-30 14:29:32 -0500

    Models Category

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model for organizing blog posts"""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', args=[self.slug])

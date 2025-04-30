598e8a17c46738c34a94b79138443a84d24ec85a
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-04-30 14:16:14 -0500

    PostDetailView

--- src/blog/views.py
+++ src/blog/views.py
@@ -22,3 +22,21 @@ class PostListView(ListView):
         context['tags'] = Tag.objects.annotate(post_count=Count('posts')).order_by('-post_count')[:10]
         context['recent_posts'] = Post.published_objects.recent_posts()
         return context
+    
+class PostDetailView(DetailView):
+    """View for displaying a single post"""
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    
+    def get_queryset(self):
+        """Get only published posts"""
+        return Post.published_objects.published()
+    
+    def get_context_data(self, **kwargs):
+        """Add additional context data"""
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['tags'] = Tag.objects.annotate(post_count=Count('posts')).order_by('-post_count')[:10]
+        context['recent_posts'] = Post.published_objects.recent_posts()
+        return context
